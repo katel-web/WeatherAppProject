8cb21a4e979337cd48bb3a4022d5dfc266914181
katel-web <katelemass@gmail.com>
2024-07-10 14:10:29 +0100

    Added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
+function refreshWeather(response) {
+  console.log(response.data);
+  let newTemp = Math.round(response.data.temperature.current);
+  //console.log(newTemp);
+  let appTemperature = document.querySelector("#app-temperature");
+  appTemperature.innerHTML = newTemp;
+  let cityName = document.querySelector("#city-name");
+  cityName.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let searchFormInput = document.querySelector("#search-form-input");
+  let cityName = searchFormInput.value;
+  let apiKey = "8ff30efd06o4d12f7ftb8b44c4ad300b";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=metric`;
+  console.log(apiURL);
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function changeCityName(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
-  let cityName = document.querySelector("#city-name");
-  cityName.innerHTML = searchFormInput.value;
+  //let cityName = document.querySelector("#city-name");
+  //cityName.innerHTML = searchFormInput.value;
+  searchCity(searchFormElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
